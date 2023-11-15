39bb8d49a4d06b8903a1234dd4c0afabfa7e568a
begalievn <begaliev.info@gmail.com>
2023-11-15 23:15:13 +0600

    Added roman-to-integer

---
+++ leetcode/romat-to-integer.js
@@ -0,0 +1,23 @@
+
+function romanToInt(s) {
+  const obj = {
+      I: 1,
+      V: 5, 
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+  };
+
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+      if (obj[s[i]] < obj[s[i+1]]) {
+          res -= obj[s[i]];
+          continue;
+      }
+      res += obj[s[i]];
+  }
+  
+  return res;
+};
