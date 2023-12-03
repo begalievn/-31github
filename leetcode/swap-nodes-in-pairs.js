bcf87adfd553267c9446dbbfadb65f940c48cd20
begalievn <begaliev.info@gmail.com>
2023-12-03 16:38:12 +0600

    Add swap nodes in pairs

---
+++ leetcode/swap-nodes-in-pairs.js
@@ -0,0 +1,12 @@
+function swapPairs(head) {
+
+  if (!head || !head.next) return head;
+
+  const curr = head;
+  const next = head.next;
+  const next_next = next.next;
+
+  curr.next = swapPairs(next_next);
+  next.next = curr;
+  return next;
+}
